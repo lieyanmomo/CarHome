f1425eaec292d23cc10f465f5c6c94db764ea97b
zhanghaibin <zhanghaibin@zhanghaibindeMacBook-Pro.local>
2016-06-18 14:31:04 +0800

    设置启动图片和APP图标

---
+++ CarHome/CarHome/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CarHome
+//
+//  Created by zhanghaibin on 16/6/18.
+//  Copyright © 2016年 zhanghaibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CarHome/CarHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarHome
+//
+//  Created by zhanghaibin on 16/6/18.
+//  Copyright © 2016年 zhanghaibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
